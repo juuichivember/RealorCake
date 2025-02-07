b818ecf703518449d0c3fb670ddeab1aed94948a
Aiyayayo <aiyyarach@gmail.com>
2025-02-07 10:03:16 +0700

    Add button class and homepage file
    homepage.py เป็นตัวเปิดหลังของโปรแกรม เดี๋ยวจะต้องสร้างไฟล์ใหม่มา set การตั้งค่าโปรแกรมหลัก

---
+++ RealOrCake/button.py
@@ -0,0 +1,32 @@
+import pygame
+
+class Button():
+    def __init__(self, x, y, image, scale):
+        # constructor
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.smoothscale(image, (int(width * scale), int(height * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.clicked = False # unclick state
+
+    def draw(self, surface):
+        # draw button on screen
+        surface.blit(self.image, (self.rect.x, self.rect.y)) # surface == screen
+
+        return self
+    
+    def IsMouseOver(self):
+        action = False
+        # get mouse position
+        pos = pygame.mouse.get_pos()
+
+        # check mouseover and clicked conditions
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False: # if left clicked
+                self.clicked = True
+                action = True
+        if pygame.mouse.get_pressed()[0] == 0: # if right clicked
+            self.clicked = False
+
+        return action
