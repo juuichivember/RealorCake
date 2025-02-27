6e9856eb45805cd3f00568e3a94c84b56bfe47a5
Aiyayayo <aiyyarach@gmail.com>
2025-02-28 00:30:27 +0700

    Add files via upload

---
+++ RealOrCake/letterTextBox.py
@@ -0,0 +1,13 @@
+class LetterTextBox():
+    text = ""
+    def __init__(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def get_text():
+        pass
+    
+    def set_text():
+        pass
