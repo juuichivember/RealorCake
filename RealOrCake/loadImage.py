dde10e7c44d1a2e70571847bd7a620f5f5b2959d
Aiyayayo <aiyyarach@gmail.com>
2025-02-28 00:31:07 +0700

    Delete RealOrCake/loadImage.py

--- RealOrCake/loadImage.py
+++
@@ -1,8 +0,0 @@
-import os
-import pygame
-
-def load_cake_image(cake_type):
-    path = f"Elements/layer7_base_1/{cake_type}.png"
-    if os.path.exists(path):
-        return pygame.image.load(path)
-    return None
