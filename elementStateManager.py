823471052366c483d5e40fef9db5067c2600cab1
Aiyayayo <aiyyarach@gmail.com>
2025-03-02 02:24:13 +0700

    Cake 0.1.0

---
+++ elementStateManager.py
@@ -0,0 +1,25 @@
+from elementState import BaseState, BCreamState, LCreamState, MCreamState, TCreamState, ToppingState
+
+class ElementStateManager:
+    def __init__(self):
+        self.states = {
+            "base": BaseState(),
+            "behindcream": BCreamState(),
+            "lowercream": LCreamState(),
+            "middlecream": MCreamState(),
+            "topcream": TCreamState(),
+            "topping": ToppingState()
+        }
+        self.current_state = "base"
+
+    def set_state(self, new_state):
+        self.current_state = new_state
+        self.states[self.current_state].enter()
+
+    def update(self):
+        next_state = self.states[self.current_state].update()
+        if next_state != self.current_state:
+            self.set_state(next_state)
+
+    def draw(self, screen):
+        self.states[self.current_state].draw(screen)
---
+++ main.py
@@ -0,0 +1,75 @@
+import pygame
+import sys
+from state import Start, RandomCake, Decoration, Score, End, Message
+from stateManager import *
+from screen import set_screen
+
+#SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
+FPS = 60
+
+# Default setup
+
+class Game():
+    def __init__(self):
+        pygame.init()
+        screen_size = pygame.display.get_desktop_sizes()
+        self.screen_w, self.screen_h = set_screen(screen_size[0])
+
+        pygame.display.set_caption("Namkhing's Cake")
+        self.screen = pygame.display.set_mode((self.screen_w, self.screen_h))
+        self.clock = pygame.time.Clock()
+
+        # Call Context's and Concrete State's Constructor
+        self.gameStateManager = GameStateManager('start')
+        self.start = Start(self.screen, self.gameStateManager, self.screen_w, self.screen_h)
+        self.random_cake = RandomCake(self.screen, self.gameStateManager, self.screen_w, self.screen_h)
+        self.decoration = Decoration(self.screen, self.gameStateManager, self.screen_w, self.screen_h)
+        self.score_page = Score(self.screen, self.gameStateManager, self.screen_w, self.screen_h)
+        self.end = End(self.screen, self.gameStateManager, self.screen_w, self.screen_h)
+        self.message = Message(self.screen, self.gameStateManager, self.screen_w, self.screen_h)
+
+        self.states = {'start': self.start, 
+                       'random_cake': self.random_cake,
+                       'decoration': self.decoration,
+                       'score_page': self.score_page,
+                       'end': self.end,
+                       'end_message': self.message
+                       }
+
+    def run(self):
+        current_state = self.gameStateManager.get_state()
+        while True:
+            mouse_pos = pygame.mouse.get_pos()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+                
+                if current_state == "end_message":
+                    self.gameStateManager.get_event().handle_event(event)
+                if self.gameStateManager.get_alert_active():
+                    print("in loop")
+                    if self.gameStateManager.alert.handle_event(event, mouse_pos):
+                        self.gameStateManager.set_alert_active(False)# alert is done
+                        print("alert set, active false")
+
+            new_state = self.gameStateManager.get_state()
+
+            if new_state != current_state:
+                self.states[new_state].enter()
+                current_state = new_state
+            
+            self.states[new_state].run()
+
+            
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
