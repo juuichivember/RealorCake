a5f052431699697c44f34a2b9d72095ba70aea59
Aiyayayo <aiyyarach@gmail.com>
2025-03-05 16:43:24 +0700

    update SaveImgButton.py
    edit a function to make directory 'Screenshots' if there is none

--- RealOrCake/saveImgButton.py
+++ RealOrCake/saveImgButton.py
@@ -23,6 +23,7 @@ class SaveImgButton(Button):
         try:
             filenames = os.listdir(directory)
         except FileNotFoundError:
+            os.makedirs(directory)
             return None  # Return None if directory not found
 
         numbers = []
