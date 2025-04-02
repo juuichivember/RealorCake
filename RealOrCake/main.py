66a47dfb74ce39d21335af69dbfccfc564e48cf3
juuichivember <aiyyarach@gmail.com>
2025-04-02 20:18:57 +0700

    update readme and icon

--- RealOrCake/main.py
+++ RealOrCake/main.py
@@ -24,11 +24,14 @@ class Game():
         screen_size = pygame.display.get_desktop_sizes()
         self.screen_w, self.screen_h = set_screen(screen_size[0])
 
-                # โหลดโลโก้
+        # โหลดโลโก้
         base_path = get_base_path() # หา path ของไฟล์
         icon_path = os.path.join(base_path, "assets", "other", "logo64.png")  # path ของโลโก้, ทำมาใหม่ให้เป็น 64x64
         icon = pygame.image.load(icon_path)  # โหลดรูปภาพ
 
+        # ตั้งโลโก้ให้เป็น icon ของหน้าต่าง
+        pygame.display.set_icon(icon)
+        
         pygame.display.set_caption("Namkhing's Cake")
         self.screen = pygame.display.set_mode((self.screen_w, self.screen_h))
         self.clock = pygame.time.Clock()
